323011e9df7adf69e373d9a13d5b6af1912c80bb
TheFreeMan <theparkinator56@gmail.com>
2018-09-19 20:40:42 -0500

    prliminary worke

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+ 
+ 
+ var guesstheletter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var rngesus = Math.floor(Math.random() * guesstheletter.length);
+var failedAttempts =
+
+document.onkeyup = function(event) {
+    var userguess = event.key;
+
+}
